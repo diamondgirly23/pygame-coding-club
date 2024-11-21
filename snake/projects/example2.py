4522f3aa4d66826e95ccd239ca99dc8528898333
Jocelyn harrott <diamondgirly23@outlook.com>
2024-11-20 22:18:08 -0700

    comments

--- snake/projects/example2.py
+++ snake/projects/example2.py
@@ -7,6 +7,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             continue
+        #add if statements to take in user input.
+        #hint look at event types and event keys.
     screen.fill("black")
 
     pygame.display.flip()
